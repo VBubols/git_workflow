a99942cb8e1f04e256e24a37f0953d582c0fef00
VBubols <vitorbubols@gmail.com>
2026-08-25 20:02:23 -0300

    feat: adicionado um catalogo de produtos

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const catalogo = [
+  { nome: "Teclado", preco: 150.0 },
+  { nome: "Mouse", preco: 80.0 },
+  { nome: "Monitor", preco: 900.0 },
+  { nome: "Gabinete", preco: 120.0 },
+  { nome: "Headset", preco: 320.0 },
+  { nome: "Cadeira Gamer", preco: 540.0 },
+];
+
 const carrinho = [];
 
 function adicionarItem(nome, preco, quantidade = 1) {
