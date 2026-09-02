3b3541e522ebd4dcdab4d4bfd30e9d27992c5135
VBubols <vitorbubols@gmail.com>
2026-09-02 20:27:24 -0300

    add all tests

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-const carrinho = [];
+const { adicionarItem, deletarItem, listarCarrinho } = require("./src/carrinho");
 
-function adicionarItem(nome, preco, quantidade = 1) {
-  carrinho.push({ nome, preco, quantidade });
-  console.log(`Adicionado ao carrinho item: ${nome}`);
-}
-
-function deletarItem(nome) {
-    const index = carrinho.findIndex(item => item.nome === nome);
-    carrinho.splice(index, 1);
-    console.log(`Removido item do carrinho: ${nome}`);
-}
-
-function calcularTotal() {
-  return carrinho.reduce((soma, item) => soma + item.preco * item.quantidade, 0);
-}
-
-function listarCarrinho() {
-  carrinho.forEach(item => {
-    console.log(`${item.nome} x${item.quantidade} = R$ ${(item.preco * item.quantidade).toFixed(2)}`);
-  });
-  console.log(`Total: R$ ${calcularTotal().toFixed(2)}`);
-}
-
-adicionarItem("Teclado", 150.00);
-adicionarItem("Mouse", 80.00, 2);
-deletarItem("Teclado");
+adicionarItem(1);
+adicionarItem(1);
+adicionarItem(2);
+deletarItem("mouse");
 listarCarrinho();
